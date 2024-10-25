813ec457ab69a802ae47070dfdfd4bb6b90c2018
reddymanyam <reddappareddy18@gmail.com>
2024-10-25 16:44:16 +0530

    initial commit

---
+++ src/components/Quoate1/Quoate1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Quoate1 = () => {
+    return (
+        <>
+
+            <div className='mx-auto text-center max-w-screen-md lg:mb-12'>
+                <h1 className='text-3xl font-extrabold pt-24 mb-4 '>Designed for business teams like yours</h1>
+                <span className='sm:text-xl font-light pt-4 '>Here at Landwind we focus on markets where technology, innovation, and captial can unlock long-term value and drive economic growth</span>
+            </div>
+        </>
+    )
+}
+
+export default Quoate1
