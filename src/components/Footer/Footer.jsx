5228a088a9782eff69c7478effe7bdbe780ae517
reddymanyam <reddappareddy18@gmail.com>
2024-10-26 13:43:30 +0530

    updated

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full'>
+            <div className='w-[94%] mx-auto grid grid-cols-2 grid-rows-3 p-1 sm:grid-cols-2 sm:grid-rows-3 sm:p-1 md:grid-cols-3 md:grid-rows-2 md:p-7 lg:grid-cols-5 lg:grid-rows-1 mb-3'>
+
+                <div className='space-y-4 mb-3' >
+                    <h5 className='font-semibold text-lg'>COMPANY</h5>
+                    <h5 className="text-gray-500">About</h5>
+                    <h5 className="text-gray-500">Careers</h5>
+                    <h5 className="text-gray-500">Brand Center</h5>
+                    <h5 className="text-gray-500">Blog</h5>
+                </div>
+
+                <div className='space-y-4 mb-3'>
+                    <h5 className='font-semibold text-lg'>HELP CENTER</h5>
+                    <h5 className="text-gray-500">Discord Server</h5>
+                    <h5 className="text-gray-500">Twitter</h5>
+                    <h5 className="text-gray-500">Facebook</h5>
+                    <h5 className="text-gray-500">Contact Us</h5>
+                </div>
+
+                <div className='space-y-4 mb-3'>
+                    <h5 className='font-semibold text-lg'>LEGAL</h5>
+                    <h5 className="text-gray-500">Privacy Policy</h5>
+                    <h5 className="text-gray-500">Licensing</h5>
+                    <h5 className="text-gray-500">Terms</h5>
+                </div>
+
+                <div className='space-y-4 mb-3'>
+                    <h5 className='font-semibold text-lg'>COMPANY</h5>
+                    <h5 className="text-gray-500">About</h5>
+                    <h5 className="text-gray-500">Careers</h5>
+                    <h5 className="text-gray-500">Brand Center</h5>
+                    <h5 className="text-gray-500">Blog</h5>
+                </div>
+
+                <div className='space-y-4'>
+                    <h5 className='font-semibold text-lg'>DOWNLOAD</h5>
+                    <h5 className="text-gray-500">iOS</h5>
+                    <h5 className="text-gray-500">Android</h5>
+                    <h5 className="text-gray-500">Windows</h5>
+                    <h5 className="text-gray-500">MacOS</h5>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
