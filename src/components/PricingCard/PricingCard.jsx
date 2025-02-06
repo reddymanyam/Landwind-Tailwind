8affa0d6f8395a2663cad6f9e4d08b2b1898323d
reddymanyam <reddappareddy18@gmail.com>
2025-02-07 00:17:26 +0530

    priccing

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -40,7 +40,6 @@ const PricingCard = () => {
                 <li className="flex space-x-2"><svg class="flex-shrink-0 w-5 h-5 mr-3 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>Free updates: 24 months</li>
                 </ul>
                 <button className='bg-purple-600 h-10 rounded-md text-white'>Get started</button>
-                
             </div>
             
             <div className='border-2 border-gray-100 p-10 flex flex-col text-center space-y-4'>
